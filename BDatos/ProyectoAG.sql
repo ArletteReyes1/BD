93e7935b857c23711579402d08ce29bfb57b68be
Paoit <paolaitz@exalumno.unam.mx>
2025-09-02 16:28:58 -0600

    Se agregan 3 registros en productos

--- BDatos/ProyectoAG.sql
+++ BDatos/ProyectoAG.sql
@@ -24,7 +24,91 @@ VALUES (null, "Gran Tour de Europa", "https://res.cloudinary.com/dte9mtmma/image
 - Ningún servicio no especificado.
 - Todas las excursiones que se mencionan como opcionales.
 - Impuestos aéreos por persona.
-NOTA: 65 EUROS QUE CORRESPONDEN A PROPINAS PARA CHOFERES, TASAS HOTELES Y MUNICIPALES. SE PAGA EN DESTINO");
+NOTA: 65 EUROS QUE CORRESPONDEN A PROPINAS PARA CHOFERES, TASAS HOTELES Y MUNICIPALES. SE PAGA EN DESTINO"),
+(null,"Especial Egipto y Dubái", 
+"https://res.cloudinary.com/dte9mtmma/image/upload/v1755824176/egiptoydubai_x1myfh.jpg",
+1299,
+"El Cairo, Aswan, Kom Ombo, Edfu, Esna, Luxar y Duboi",
+"15",
+"12",
+"-Boleto de avión en viaje redondo México – Dubái – México, en clase turista.
+-Vuelos Dubái – El Cairo - Dubái / El Cairo – Aswan / Luxor – El Cairo en clase turista.
+- 3 noches de alojamiento en el Cairo
+-3 noches de crucero por el Nilo
+-1 noche de alojamiento en Luxor
+-5 noches de alojamiento en Dubái.
+-Régimen alimenticio indicado en el itinerario.
+-Traslados indicados.
+-Visitas indicadas.
+-Guías de habla hispana.
+-Autocar con aire acondicionado.",
+"-Gastos personales y extras en los hoteles.
+-Propinas en Egipto: 60 USD por persona. (Se paga directo en destino)
+-Propinas en Dubái: 35 USD por persona. (Se paga directo en destino)
+-Impuesto Turismo en Dubái: 5 USD por habitación por noche. (Se paga directo en destino)BookingConclusion
+-Impuestos aéreos
+-Visa de Egipto: 25 USD aproximadamente (se paga en destino)
+-Ningún servicio no especificado como incluido o como opcional.
+-Seguro de viaje (Es obligatorio para entrar a los Emiratos árabes Unidos)."),
+(null, "Tailandia y Phuket", 
+"https://res.cloudinary.com/dte9mtmma/image/upload/v1755824181/tailandia-phukec_saz3lm.jpg", 
+1599,
+ "Bangkok, Kanchanaburi, Chiang Rai, Chiang Mai, Phuket", 
+ "13", "9", 
+ "- Boleto de avión en viaje redondo México – Bangkok / Phuket – México en clase turista.
+- Boleto de avión Bangkok – Chiang Rai / Chiang Mai – Phuket en clase turista.
+- 03 noches de alojamiento en Bangkok.
+- 02 noche de alojamiento en Chiang Rai.
+- 02 noches de alojamiento en Chiang Mai.
+- 02 noches de alojamiento en Phuket.
+- Traslados indicados
+- Visitas indicadas
+- Guías de habla hispana
+- Autocar con aire acondicionado.",
+ "-Gastos personales y extras en los hoteles.
+-Propinas en Tailandia: 50 USD por persona (Se paga directo en destino)
+-Ningún servicio no especificado como incluido o especificado como opcional.-Visa de Tailandia-Impuestos aéreos por persona"),
+(null,"Tailandia y Phunket EXPRESS","https://res.cloudinary.com/dte9mtmma/image/upload/v1755824174/tailandiaExpress_jmrxsp.jpg",
+ 999,
+ "Bangkok, Ayutthaya, Lopburi, Phitsanuloke, Sukhothai, Chiang Rai, Chiang Mai, Phuket.",
+ "11","7",
+ "- Boleto de avión en viaje redondo México -Bangkok - México, en clase tusrita.
+- Boleto de avión Chiang Mai - Phuket en clase turista.
+- Boleto de avión Phuket - Bangkok en clase turista.
+- 2 noches de alojamiento en Bangkpk.
+- 1 noche de alojamiento en Phitsanuloke.
+- 1 noche de alojamiento en Chiang Rai.
+- 1 noche de alojamiento en Chiang Mai.
+- 2 noches de alojamiento en Phuket.
+- Régimen alimenticio indicado en itinerario.
+- Autocar con aire acondicionado.",
+"- Gastos personales y extras en hoteles, BebidasPropinas para maleteros, camaristas, meseros, etc.
+-Propinas en Tailandia: 50 USD por persona (Se paga directo en destino)
+-Gastos estras en los hoteles como llamadas telefónicas, lavandería, etc.
+-Visa de Tailanda.
+-Fee de cámaras en los monumentos.
+-Ningún servicio no específicado como incluido o especificado como opcional, Impuestos aéreos"),
+(null,"Tailandia y Phuket",
+"https://res.cloudinary.com/dte9mtmma/image/upload/v1755824181/tailandia-phukec_saz3lm.jpg",
+"1599",
+"Bangkok, Kanchanaburi, Chiang Rai, Chiang Mai, Phuket",
+"13","9",
+"- Boleto de avión en viaje redondo México – Bangkok / Phuket – México en clase turista.
+- Boleto de avión Bangkok – Chiang Rai / Chiang Mai – Phuket en clase turista.
+- 03 noches de alojamiento en Bangkok.
+- 02 noche de alojamiento en Chiang Rai.
+- 02 noches de alojamiento en Chiang Mai.
+- 02 noches de alojamiento en Phuket.
+- Traslados indicados
+- Visitas indicadas
+- Guías de habla hispana
+- Autocar con aire acondicionado.",
+"-Gastos personales y extras en los hoteles.
+-Propinas en Tailandia: 50 USD por persona (Se paga directo en destino)
+-Ningún servicio no especificado como incluido o especificado como opcional.
+-Visa de Tailandia
+-Impuestos aéreos por persona");
+
 
 SELECT * FROM  Cotizacion;
 

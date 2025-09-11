622a542df6a441acf91a2c47a5b8788e3cf42fd3
Nadia Bustos Vazquez <nadiamickey.81@gmail.com>
2025-09-11 08:33:25 -0600

    Se agrego en static el front para unificar proyecto

--- src/main/java/com/agenciaviajes/agenciaviajes/controller/ProductosController.java
+++ src/main/java/com/agenciaviajes/agenciaviajes/controller/ProductosController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path="/api/Productos/") //http://localhost:8080/api/Productos/
-@CrossOrigin(origins="*", methods= {RequestMethod.GET, RequestMethod.POST})
+//@CrossOrigin(origins="*", methods= {RequestMethod.GET, RequestMethod.POST})
 public class ProductosController {
 
 	private final ProductosService service;
